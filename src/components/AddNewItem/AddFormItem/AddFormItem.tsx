e6ab45df72ad2217d0ef10507bd7b0666e1d915c
unknown <guerreiramaya2016@gmail.com>
2021-05-31 00:52:09 -0300

    chore: resolvendo acerca do build

--- src/components/AddNewItem/AddFormItem/AddFormItem.tsx
+++ src/components/AddNewItem/AddFormItem/AddFormItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from '../../Button/Button';
-import ResizeTextarea from '../../ResizeTextarea/ResizeTextarea';
+import ResizeTextarea from '../../ResizeTextarea';
 
 interface AddFormItemProps {
 	placeholder: string;
