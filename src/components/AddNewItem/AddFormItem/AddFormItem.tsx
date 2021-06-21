01ba81c0e568f337a0e4e33674c72729d4827789
Jenni <guerreiramaya2016@gmail.com>
2021-06-20 22:45:47 -0300

    chore: realizando alterações

--- src/components/AddNewItem/AddFormItem/AddFormItem.tsx
+++ src/components/AddNewItem/AddFormItem/AddFormItem.tsx
@@ -1,68 +1,82 @@
-import React from 'react';
-import Button from '../../Button/Button';
-import ResizeTextarea from '../../ResizeTextarea/ResizeTextarea';
+import React from "react";
+import Button from "../../Button/Button";
+import ResizeTextarea from "../../ResizeTextArea/ResizeTextArea";
 
 interface AddFormItemProps {
-	placeholder: string;
-	placeholder2: string;
-	textButton: string;
-	variant: string;	
-	text: string;	
-	text2: string;
-	handelSubmit: () => void;
-	onChangeText: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-	onChangeText2: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-	showHandler: () => void;
+  placeholder: string;
+  placeholder2: string;
+  textButton: string;
+  variant: string;
+  text: string;
+  text2: string;
+  handelSubmit: () => void;
+  onChangeText: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  onChangeText2: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  showHandler: () => void;
 }
 
 const AddFormItem = (props: AddFormItemProps) => {
-	const {text, text2, placeholder, placeholder2, textButton, variant, handelSubmit, onChangeText,onChangeText2, showHandler} = props;
-	
+  const {
+    text,
+    text2,
+    placeholder,
+    placeholder2,
+    textButton,
+    variant,
+    handelSubmit,
+    onChangeText,
+    onChangeText2,
+    showHandler,
+  } = props;
 
-	const onEnterPress = (e: React.KeyboardEvent) => {
-		if (e.keyCode === 13 && e.shiftKey === false) {
-			e.preventDefault();
-			handelSubmit();
-		}
-	};
+  const onEnterPress = (e: React.KeyboardEvent) => {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      e.preventDefault();
+      handelSubmit();
+    }
+  };
 
-	return (
-		<div>
-			<form onSubmit={handelSubmit}>
-				<div className='add-item__form'>
-					{variant === 'column' ? (
-						<input
-							value={text}
-							className='card add-item__input'							
-							onChange={onChangeText}
-							placeholder={placeholder}
-							type='text'
-							autoFocus
-						/>
-					) : (
-						<>
-							<ResizeTextarea 
-								value={text}					
-								onChange={onChangeText}
-								onKeyDown={onEnterPress}						
-								placeholder={placeholder}
-							/>
-							<ResizeTextarea 
-								value={text2}					
-								onChange={onChangeText2}
-								onKeyDown={onEnterPress}						
-								placeholder={placeholder2}
-							/>	
-						</>					
-					)}
-					<div className='add-item__btn'>
-						<Button type='submit'>{textButton} </Button>
-						<div onClick={showHandler} className='icon-close'></div>
-					</div>
-				</div>
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      <form onSubmit={handelSubmit}>
+        <div className="add-item__form">
+          {variant === "column" ? (
+            <input
+              value={text}
+              className="card add-item__input"
+              onChange={onChangeText}
+              placeholder={placeholder}
+              type="text"
+              autoFocus
+            />
+          ) : (
+            <>
+              <ResizeTextarea
+                value={text}
+                onChange={onChangeText}
+                onKeyDown={onEnterPress}
+                placeholder={placeholder}
+              />
+              <ResizeTextarea
+                value={text2}
+                onChange={onChangeText2}
+                onKeyDown={onEnterPress}
+                placeholder={placeholder2}
+              />
+            </>
+          )}
+          <div className="add-item__btn">
+            <Button type="submit">{textButton} </Button>
+            <div onClick={showHandler} className="icon-close"></div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddFormItem;
